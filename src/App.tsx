b29f099cbb6af6371f665f7b8d01d6c690fe0f6e
Daniel Khankin <wildeyes@users.noreply.github.com>
2020-07-07 18:59:11 +0300

    Add react router and connect page.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,22 @@ import './App.scss'
 import React from 'react'
 
 import Altflow from './Altflow'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ConnectPage from './lib/pages/ConnectPage'
 
 const App = () => {
-	return <Altflow />
+	return (
+		<Router>
+			<Switch>
+				<Route path="/login">
+					<ConnectPage />
+				</Route>
+				<Route path="/">
+					<Altflow />
+				</Route>
+			</Switch>
+		</Router>
+	)
 }
 
 export default App
---
+++ src/lib/pages/ConnectPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ConnectDrivePage = () => {
+	return (
+		<div>
+			<h1>Welcome : AltFlow</h1>
+			<p>TBD Login / Signup</p>
+		</div>
+	)
+}
+
+export default ConnectDrivePage
